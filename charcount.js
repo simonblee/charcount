25aa9e32c78d3a0cb98aca5ffaf1af035ea3769e
Simon Blee <simblee@gmail.com>
2013-05-15 11:27:57 +0800

    Fixed formatting, tabs.

--- charcount.js
+++ charcount.js
@@ -1,36 +1,43 @@
-( function ( $ ){
-	
-	$.fn.charcount = function ( options ) {
+(function ($) {
 
-		return this.each( function () {  
-			var counter,
-				mkp = '<span class="charcount"></span>',
-				$this = $(this);
+    $.fn.charcount = function (options) {
 
-			// Overwrite defaults
-			defaults = {
-				tmpl: '{count}/{max}',
-				max: $this.attr('maxlength'),
-				appendTo: null
-			};
-			options = $.extend( {}, defaults, options );
-			$this.attr('maxlength', options.max ); // In-case not set, use browser maxchar limiter
+        return this.each(function () {
+            var counter,
+                mkp = '<span class="charcount"></span>',
+                $this = $(this);
 
-			// Create markup
-			counter = options.appendTo ? $(mkp).appendTo( options.appendTo ) : $(mkp).insertAfter( $this );
+            // Overwrite defaults
+            defaults = {
+                template: '{count}/{max}',
+                max: $this.attr('maxlength'),
+                appendTo: null
+            };
+            options = $.extend({}, defaults, options);
+            $this.attr('maxlength', options.max); // In-case not set, use browser maxchar limiter
 
-			// Bind the keyup event 
-			$this.on('keyup.charcount', function (){
-				var count = $this.val().length, 
-					remaining = parseInt( options.max ) - count;
-				if( remaining <= 0 ){
-					$this.val( $this.val().substr( 0, options.max ) );
-				}
+            // Create markup
+            counter = options.appendTo ? $(mkp).appendTo(options.appendTo) : $(mkp).insertAfter($this);
 
-				// Apply to template
-				counter.text( options.tmpl.replace( '{count}', count ).replace( '{max}', options.max ).replace( '{remaining}', remaining ) );
-			}).trigger('keyup.charcount');
-		});
-	};
+            // Bind the keyup event
+            $this.on('keyup.charcount', function () {
+                var text, replace,
+                    template = options.template,
+                    count = $this.val().length,
+                    remaining = parseInt( options.max ) - count;
 
-})( jQuery );
+                if (remaining <= 0) {
+                    $this.val($this.val().substr(0, options.max));
+                }
+
+                // Apply to template
+                counter.text(
+                    options.template.replace('{count}', count)
+                                    .replace('{max}', options.max)
+                                    .replace('{remaining}', remaining)
+                );
+            }).trigger('keyup.charcount');
+        });
+    };
+
+})(jQuery);
