b7a0502d6fb5badb24c88249d5617023332ac607
Jared <jared.b.ross@gmail.com>
2013-10-11 12:17:06 +0800

    Change to just pass in the counter element instead of creating it + Minor changes

--- charcount.js
+++ charcount.js
@@ -3,35 +3,31 @@
     $.fn.charcount = function (options) {
 
         return this.each(function () {
-            var counter,
-                mkp = '<span class="charcount"></span>',
+            var $counter,
                 $this = $(this);
 
             // Overwrite defaults
             defaults = {
                 template: '{count}/{max}',
                 max: $this.attr('maxlength'),
-                appendTo: null
+                $element: null
             };
             options = $.extend({}, defaults, options);
             $this.attr('maxlength', options.max); // In-case not set, use browser maxchar limiter
 
-            // Create markup
-            counter = options.appendTo ? $(mkp).appendTo(options.appendTo) : $(mkp).insertAfter($this);
+            $counter = options.$element;
 
             // Bind the keyup event
             $this.on('keyup.charcount', function () {
-                var text, replace,
-                    template = options.template,
-                    count = $this.val().length,
-                    remaining = parseInt( options.max ) - count;
+                var count = $this.val().length,
+                    remaining = parseInt(options.max, 10) - count;
 
                 if (remaining <= 0) {
                     $this.val($this.val().substr(0, options.max));
                 }
 
                 // Apply to template
-                counter.text(
+                $counter.text(
                     options.template.replace('{count}', count)
                                     .replace('{max}', options.max)
                                     .replace('{remaining}', remaining)
